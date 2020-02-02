9b1f0d1e59a396e32e7924e8cb4c9eaf40bf7401
Hilda <eskarina@eskarina.ca>
2020-02-01 17:45:45 -0800

    created simple part covering system

--- Main.gd
+++ Main.gd
@@ -2,4 +2,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_node("./RobotPart2").set_coverable(true)
+	# set layers
+	get_node("./RobotChest").part_layer = 1
+	get_node("./RobotChestplateLeft").part_layer = 0
+	get_node("./RobotChestplateRight").part_layer = 0
